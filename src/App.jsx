07503a6b9c4ce40a125e8e4abb21a7d528215118
ketchup0211 <dlcka1111@naver.com>
2025-12-19 23:10:41 +0900

    [ADD] StarTree

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import StarTree from "./utils/StarTree";
 import TypingEffect from "./utils/TypingEffect";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
-        <h1>Merry Christmas</h1>
+        <h1>🎄 Merry Christmas 🎄</h1>
+        <StarTree />
         <TypingEffect
           text={"We wish you a merry christmas\n and a happy New 2026 !"}
         />
---
+++ src/utils/StarTree.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function StarTree({
+  height = 5,
+  startStars = 1,
+  step = 2,
+  center = true,
+}) {
+  const lines = Array.from({ length: height }, (_, i) => startStars + i * step);
+  const maxStars = lines[lines.length - 1];
+
+  return (
+    <div style={{ fontFamily: "monospace", lineHeight: 1.2 }}>
+      {lines.map((starCount, idx) => {
+        const pad = center ? " ".repeat((maxStars - starCount) / 2) : "";
+        const text = pad + "*".repeat(starCount);
+
+        return (
+          <React.Fragment key={idx}>
+            {text}
+            <br />
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+}
