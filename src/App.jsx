f1b92baf8858f523d91b1326f58536cffd870728
ketchup0211 <dlcka1111@naver.com>
2025-12-19 22:24:51 +0900

    [ADD] typing text

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import TypingEffect from "./utils/TypingEffect";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,9 @@ function App() {
     <>
       <div>
         <h1>Merry Christmas</h1>
+        <TypingEffect
+          text={"We wish you a merry christmas\n and a happy New 2026 !"}
+        />
       </div>
     </>
   );
